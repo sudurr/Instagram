b8758a976fc1b1d372f699e4f8db0d18f2cef2f4
Sudur Sugunushev <sudursugunushev@gmail.com>
2023-01-30 13:52:35 +0600

    4th part added

--- Instagram/Instagram/Feed/FeedViewController.swift
+++ Instagram/Instagram/Feed/FeedViewController.swift
@@ -22,12 +22,27 @@ private extension FeedViewController {
         view.backgroundColor = .white
         navigationController?.navigationBar.tintColor = .black
         navigationItem.leftBarButtonItems = makeLeftBarButtonItems()
+        navigationItem.rightBarButtonItems = makeRightBarButtonItems()
     }
     
     func makeLeftBarButtonItems() -> [UIBarButtonItem] {
         let logoBarButtonItem = UIBarButtonItem(customView: LogoView())
         let dropDownButtonItem = UIBarButtonItem(title: nil, image: UIImage(systemName: "chevron.down"), target: self, action: nil, menu: makeDropDownMenu())
         return [logoBarButtonItem, dropDownButtonItem]
+    }
+    
+    func makeRightBarButtonItems() -> [UIBarButtonItem] {
+        let addBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus.app"), style: .plain, target: self, action: #selector(didTapPlusButton))
+        let directBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "paperplane"), style: .plain, target: self, action: #selector(didTapDirectButton))
+        return [directBarButtonItem, addBarButtonItem]
+    }
+
+    @objc func didTapPlusButton() {
+        print("Add")
+    }
+
+    @objc func didTapDirectButton() {
+        print("Direct")
     }
     
     func makeDropDownMenu () -> UIMenu {
